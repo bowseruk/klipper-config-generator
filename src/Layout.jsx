99b8c6e661139d7cc5d26b9de976880824dbffb9
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-07-25 22:15:32 +0100

    Improved navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,12 +4,34 @@ function Layout(props) {
     return (
         <>
             <header>
-                <nav className="navbar navbar-expand-lg bg-body tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                            <img src={logo} width="30" height="30" class="d-inline-block align-top" alt="" />
-                            Klipper Config Generator
-                        </a>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="brand logo" />
+                        Klipper Config Generator
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Printers
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a className="dropdown-item" href="#">Add</a>
+                                    <a className="dropdown-item" href="#">Another action</a>
+                                    <div className="dropdown-divider"></div>
+                                    <a className="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </header>
