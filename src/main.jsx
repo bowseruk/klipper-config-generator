4a4c9aa5c9706fc4526039bae9a2b10c3711f089
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-07-25 23:59:38 +0100

    Fixed base

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Bootstrap CSS
-// import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-// import "bootstrap/dist/js/bootstrap.bundle.min";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,16 +7,15 @@ import {
 import Layout from './Layout.jsx';
 import App from './App.jsx';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout><App /></Layout>,
-//   },
-// ], {basename: "/klipper-config-generator/"});
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><App /></Layout>,
+  },
+], {basename: "/klipper-config-generator/"});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     {/* <RouterProvider router={router} /> */}
-     <Layout><App /></Layout>
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
